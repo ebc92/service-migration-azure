1886c55ad76c303e3d8b68eddeb6d8965495f2e2
ebc92 <emil.claussen@gmail.com>
2017-02-06 14:38:21 +0100

    Configure-PSRemoting now includes pre-existing values.

--- Support/Configure-PSRemoting.ps1
+++ Support/Configure-PSRemoting.ps1
@@ -3,6 +3,11 @@ Param($computer)
 Process {
     Enable-PSRemoting -Force
     $val = (get-item wsman:\localhost\Client\TrustedHosts).value
+    If($val){
+        set-item wsman:\localhost\Client\TrustedHosts -value "$val, $computer" -Force
+    } Else {
     set-item wsman:\localhost\Client\TrustedHosts -value "$computer" -Force
+    }
+    Restart-Service WinRM
 }
 }
