f344d140ac583d537b18c6d627cde98f0b0501e6
ebc92 <emil.claussen@gmail.com>
2017-02-06 13:43:20 +0100

    Added a function to configure and verify a new DNS address.

---
+++ ADDC/Configure-DomainDNS.ps1
@@ -0,0 +1,32 @@
+﻿Function Configure-DomainDNS {
+
+<#
+  .SYNOPSIS
+  Configure and verify new DNS addresses.
+  .DESCRIPTION
+  Assumes there is only 1 valid ethernet interface.
+  .EXAMPLE
+  Example syntax
+  #>
+
+Param($addresses)
+
+Begin {
+}
+
+Process {
+    $interface = Get-NetAdapter | Select ifIndex,InterfaceDescription
+    #Logging
+    Write-Host "Configuring DNS on adapter $($interface[0].InterfaceDescription)"
+
+    Set-DnsClientServerAddress -InterfaceIndex $interface.ifIndex -ServerAddresses($addresses)
+
+    $dns = Get-DnsClientServerAddress | Select InterfaceIndex,AddressFamily,ServerAddresses
+    foreach ($element in $dns) {
+        If ($element.InterfaceIndex -eq $interface[0].ifIndex -and $element.AddressFamily -eq 2){
+            #Logging
+            Write-Host $element.ServerAddresses
+        }
+    }
+}
+}
