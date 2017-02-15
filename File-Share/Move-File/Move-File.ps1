fd40e87e88aa420006f1f375a2b8fcf4d986d15f
Gholie <nikolai.tleira@gmail.com>
2017-02-15 15:11:57 +0100

    Script works as it should, but not supernice yet

--- File-Share/Move-File/Move-File.ps1
+++ File-Share/Move-File/Move-File.ps1
@@ -6,10 +6,11 @@
 
 Function Move-File {
 Param(
-    [parameter(Mandatory=$true)]$SourcePath,
-    [parameter(Mandatory=$true)]$DestPath,
     [parameter(Mandatory=$true)]$SourceComputer,
     [parameter(Mandatory=$true)]$TarComputer,
+    $username,
+    $SourcePath,
+    $DestPath,
     $Credential,
     $RobCopyArgs,
     $MoveLog,
@@ -18,8 +19,11 @@ Param(
     $MoveFile
     )
 Process {
-    $Credential = Get-Credential
-    Deploy-FileShare($TarComputer, $Credential)
+    $username = Read-Host("Username plx")    
+    $pw = Read-Host("password plx") -AsSecureString
+    $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $username,$pw
+
+    Deploy-FileShare -TarComputer $TarComputer -credential $Credential
     #Do until loop that checks if the Path is a container, and valid
     do { 
         $SourcePath = Read-Host('Please input the source path for your network share, ie \\fileshare')
@@ -33,36 +37,33 @@ Process {
 
     Write-Verbose -Message "$SourcePath is valid, checking destination path next"
 
-    do { 
+   # do { 
         $DestPath = Read-Host('Please input the destination path for your network share, ie \\fileshare')
-        If ([string]::IsNullOrEmpty($DestPath)) {
+        <#If ([string]::IsNullOrEmpty($DestPath)) {
             $DestPath = "SomeString"
         } else {
             #DoNothing
         }
-    } until (Test-Path($DestPath) -PathType Container)
+    } until (Test-Path($DestPath) -PathType Container) #>
 
     Write-Verbose -Message "Verification OK, moving files from $SourcePath to $DestPath"
 
     #Writes all info to a $RobCopyArgs, and starts robocopy. This also saves a logfile locally to C:\ServerMigrationLogs\File-Shares
-    $MovLog = "RoboCopy-$Date.log"
-    $LogPath = "C:\ServerMigrationLogs\File-Shares\$MovLog"
-    $RobCopyArgs = "/MT /Z /E /COPYALL /R:60 /W:1 /V /TEE /log:$LogPath"
     $Date = (Get-Date -Format ddMMMyyyy_HHmm).ToString()
+    $MovLog = "RoboCopy-$Date.log"
+    $LogPath = "C:\ServerMigrationLog\File-Share\$MovLog"
+    $RobCopyArgs = "/MT /Z /E /COPYALL /R:60 /W:1 /V /TEE /log:$LogPath"
     $MoveFile = "$SourcePath $DestPath $RobCopyArgs"
 
     Write-Verbose -Message "Running robocopy with the following args: $MoveFile"
-
-    Invoke-Command -ComputerName $using:SourceComputer -Credential $using:credential -ScriptBlock {
-        Param(
-        $MoveFile
-        )
-        Start-Process robocopy -NoNewWindow -Wait -args "$MoveFile" } -ArgumentList $MoveFile
+    Start-Process robocopy -args "$MoveFile"
+    #Invoke-Command -ComputerName $SourceComputer -Credential $credential -ScriptBlock {
+    #    Start-Process -runas "$using:Credential" robocopy -args "$using:MoveFile" }
     }
 }
 
 Workflow Deploy-FileShare {
-    param(
+    Param(
     [parameter(Mandatory)]$TarComputer,
     [parameter(Mandatory)]$Credential
     )
