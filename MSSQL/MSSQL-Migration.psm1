9a489d3080b6d2ba6a450c8c937917e2fe251817
ebc92 <emil.claussen@gmail.com>
2017-02-22 17:38:14 +0100

    Scripted MSSQL deployment using config-file.

--- MSSQL/MSSQL-Migration.psm1
+++ MSSQL/MSSQL-Migration.psm1
@@ -20,9 +20,7 @@ $sLogFile = Join-Path -Path $sLogPath -ChildPath $sLogName
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
 
 Function Start-MSSQLMigrationProcess{
-  Param(
-    $InstanceName
-  )
+  Param()
   
   Begin{
     Log-Write -LogPath $sLogFile -LineValue "Starting the MSSQL migration process.."
@@ -32,8 +30,20 @@ Function Start-MSSQLMigrationProcess{
   Process{
     Try{
       $ConfigPath = Get-ChildItem -Path 'C:\Program Files\Microsoft SQL Server' -Filter 'ConfigurationFile.ini' -Recurse
-
-      Backup-SqlDatabase -ServerInstance "$env:COMPUTERNAME\$InstanceName"
+      New-PSDrive -PSProvider FileSystem -Name "share" -Root \\158.38.43.115\C$\Share -Credential $Credential -ErrorAction Stop
+      Copy-Item -Path $ConfigPath.FullName -Destination 'share:\'
+      
+      <# Migration by backup
+      cd SQLServer:\SQL\$env:COMPUTERNAME  
+      $instances = Get-childitem   
+        # loop through each instances and backup up all the  databases -filter out tempdb and model databases  
+  
+        foreach ($instance in $instances)  {  
+            $path = "sqlserver:\sql\$($instance.name)\databases"  
+            $alldatabases = get-childitem -Force -path $path |Where-object {$_.name -ne "tempdb" -and $_.name -ne "model"}   
+  
+            $alldatabases | Backup-SqlDatabase -BackupContainer "C:\Program Files\Microsoft SQL Server\MSSQL13.AMSTELSQL\MSSQL\Backup" -Compression On 
+        } #>  
     }
     
     Catch{
@@ -49,7 +59,20 @@ Function Start-MSSQLMigrationProcess{
     }
   }
 }
+
+Function Start-MSSQLDeployment{
+    Param(
+    [string]$Password, 
+    [string]$ConfigPath)
+    #Mount-DiskImage -ImagePath \\158.38.43.115\share\en_sql_server_2016_enterprise_x64_dvd_8701793.iso
+    $ConfigFile = 'C:\Users\Administrator.AMSTEL\Desktop\ConfigurationFile.ini'
+    $Drive = Get-WMIObject Win32_Volume -Filter "Label='SQL2016_x64_ENU'"
+    $InstallConfig = @("/ConfigurationFile=$ConfigPath", "/SAPWD=$Password")
+    $InstallSQL = "$($Drive.Caption)setup.exe" 
+    & $InstallSQL $InstallConfig
+
+}
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 Log-Start -LogPath $sLogPath -LogName $sLogName -ScriptVersion $sScriptVersion
-Start-MSSQLMigrationProcess
+Start-MSSQLMigrationProcess -InstanceName "AMSTELSQL"
 Log-Finish -LogPath $sLogFile -NoExit $True
