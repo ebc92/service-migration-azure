fbfb3a28a6c8a380523e318d368a23ff5f5e30d0
Gholie <nikolai.tleira@gmail.com>
2017-02-24 04:18:17 +0100

    Planned script structure set

---
+++ Exchange/Exchange-Migration.ps1
@@ -0,0 +1,287 @@
+﻿#requires -version 2
+<#
+    .SYNOPSIS
+    <Overview of script>
+
+    .DESCRIPTION
+    <Brief description of script>
+
+    .PARAMETER <Parameter_Name>
+    <Brief description of parameter input required. Repeat this attribute if required>
+
+    .INPUTS
+    <Inputs if any, otherwise state None>
+
+    .OUTPUTS
+    <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
+
+    .NOTES
+    Version:        1.0
+    Author:         Nikolai Thingnes Leira & Emil Claussen
+    Creation Date:  24.02.2017
+    Purpose/Change: Set structure of script
+  
+    .EXAMPLE
+    <Example goes here. Repeat this attribute for more than one example>
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = "SilentlyContinue"
+
+#Dot Source required Function Libraries
+. "C:\Scripts\Functions\Logging_Functions.ps1"
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#Script Version
+$sScriptVersion = "1.0"
+
+#Log File Info
+$sLogPath = "C:\Windows\Temp"
+$sLogName = "<script_name>.log"
+$sLogFile = Join-Path -Path $sLogPath -ChildPath $sLogName
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+Function Install-Exchange{
+  Param()
+  
+  Begin{
+    Log-Write -LogPath $sLogFile -LineValue "<description of what is going on>..."
+  }
+  
+  Process{
+    Try{
+      <code goes here>
+    }
+    
+    Catch{
+      Log-Error -LogPath $sLogFile -ErrorDesc $_.Exception -ExitGracefully $True
+      Break
+    }
+  }
+  
+  End{
+    If($?){
+      Log-Write -LogPath $sLogFile -LineValue "Completed Successfully."
+      Log-Write -LogPath $sLogFile -LineValue " "
+    }
+  }
+}
+
+Function Set-ClientAccess{
+  Param()
+  
+  Begin{
+    Log-Write -LogPath $sLogFile -LineValue "<description of what is going on>..."
+  }
+  
+  Process{
+    Try{
+      <code goes here>
+    }
+    
+    Catch{
+      Log-Error -LogPath $sLogFile -ErrorDesc $_.Exception -ExitGracefully $True
+      Break
+    }
+  }
+  
+  End{
+    If($?){
+      Log-Write -LogPath $sLogFile -LineValue "Completed Successfully."
+      Log-Write -LogPath $sLogFile -LineValue " "
+    }
+  }
+}
+
+Function Set-Transport{
+  Param()
+  
+  Begin{
+    Log-Write -LogPath $sLogFile -LineValue "<description of what is going on>..."
+  }
+  
+  Process{
+    Try{
+      <code goes here>
+    }
+    
+    Catch{
+      Log-Error -LogPath $sLogFile -ErrorDesc $_.Exception -ExitGracefully $True
+      Break
+    }
+  }
+  
+  End{
+    If($?){
+      Log-Write -LogPath $sLogFile -LineValue "Completed Successfully."
+      Log-Write -LogPath $sLogFile -LineValue " "
+    }
+  }
+}
+
+Function Set-MailBoxDatabase{
+  Param()
+  
+  Begin{
+    Log-Write -LogPath $sLogFile -LineValue "<description of what is going on>..."
+  }
+  
+  Process{
+    Try{
+      <code goes here>
+    }
+    
+    Catch{
+      Log-Error -LogPath $sLogFile -ErrorDesc $_.Exception -ExitGracefully $True
+      Break
+    }
+  }
+  
+  End{
+    If($?){
+      Log-Write -LogPath $sLogFile -LineValue "Completed Successfully."
+      Log-Write -LogPath $sLogFile -LineValue " "
+    }
+  }
+}
+
+Function Sync-ClientAccess{
+  Param()
+  
+  Begin{
+    Log-Write -LogPath $sLogFile -LineValue "<description of what is going on>..."
+  }
+  
+  Process{
+    Try{
+      <code goes here>
+    }
+    
+    Catch{
+      Log-Error -LogPath $sLogFile -ErrorDesc $_.Exception -ExitGracefully $True
+      Break
+    }
+  }
+  
+  End{
+    If($?){
+      Log-Write -LogPath $sLogFile -LineValue "Completed Successfully."
+      Log-Write -LogPath $sLogFile -LineValue " "
+    }
+  }
+}
+
+Function Sync-Transport{
+  Param()
+  
+  Begin{
+    Log-Write -LogPath $sLogFile -LineValue "<description of what is going on>..."
+  }
+  
+  Process{
+    Try{
+      <code goes here>
+    }
+    
+    Catch{
+      Log-Error -LogPath $sLogFile -ErrorDesc $_.Exception -ExitGracefully $True
+      Break
+    }
+  }
+  
+  End{
+    If($?){
+      Log-Write -LogPath $sLogFile -LineValue "Completed Successfully."
+      Log-Write -LogPath $sLogFile -LineValue " "
+    }
+  }
+}
+
+Function Sync-MailBox{
+  Param()
+  
+  Begin{
+    Log-Write -LogPath $sLogFile -LineValue "<description of what is going on>..."
+  }
+  
+  Process{
+    Try{
+      <code goes here>
+    }
+    
+    Catch{
+      Log-Error -LogPath $sLogFile -ErrorDesc $_.Exception -ExitGracefully $True
+      Break
+    }
+  }
+  
+  End{
+    If($?){
+      Log-Write -LogPath $sLogFile -LineValue "Completed Successfully."
+      Log-Write -LogPath $sLogFile -LineValue " "
+    }
+  }
+}
+
+Function Test-MailFlow{
+  Param()
+  
+  Begin{
+    Log-Write -LogPath $sLogFile -LineValue "<description of what is going on>..."
+  }
+  
+  Process{
+    Try{
+      <code goes here>
+    }
+    
+    Catch{
+      Log-Error -LogPath $sLogFile -ErrorDesc $_.Exception -ExitGracefully $True
+      Break
+    }
+  }
+  
+  End{
+    If($?){
+      Log-Write -LogPath $sLogFile -LineValue "Completed Successfully."
+      Log-Write -LogPath $sLogFile -LineValue " "
+    }
+  }
+}
+
+Function Uninstall-OldExchange{
+  Param()
+  
+  Begin{
+    Log-Write -LogPath $sLogFile -LineValue "<description of what is going on>..."
+  }
+  
+  Process{
+    Try{
+      <code goes here>
+    }
+    
+    Catch{
+      Log-Error -LogPath $sLogFile -ErrorDesc $_.Exception -ExitGracefully $True
+      Break
+    }
+  }
+  
+  End{
+    If($?){
+      Log-Write -LogPath $sLogFile -LineValue "Completed Successfully."
+      Log-Write -LogPath $sLogFile -LineValue " "
+    }
+  }
+}
+
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+Log-Start -LogPath $sLogPath -LogName $sLogName -ScriptVersion $sScriptVersion
+Script Execution goes here
+Log-Finish -LogPath $sLogFile
