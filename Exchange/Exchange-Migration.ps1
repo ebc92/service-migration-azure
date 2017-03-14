353bf7194c29f97612c3d2dfba6bd731b2c37b87
Gholie <nikolai.tleira@gmail.com>
2017-03-14 13:53:11 +0100

    Foreach loop with progress for bits transfer

--- Exchange/Exchange-Migration.ps1
+++ Exchange/Exchange-Migration.ps1
@@ -28,22 +28,22 @@
 #---------------------------------------------------------[Initialisations]--------------------------------------------------------
 
 #Set Error Action to Silently Continue
-$ErrorActionPreference = "SilentlyContinue"
+$ErrorActionPreference = 'SilentlyContinue'
 
 #Dot Source required Function Libraries
-. "C:\Scripts\Functions\Logging_Functions.ps1"
+. 'C:\Scripts\Functions\Logging_Functions.ps1'
 
 #Define all variables during testing, remove for production
-$fileshare = \\testsrv-exchang\share
+$fileshare = 'c:\tempExchange'
 
 #----------------------------------------------------------[Declarations]----------------------------------------------------------
 
 #Script Version
-$sScriptVersion = "1.0"
+$sScriptVersion = '1.0'
 
 #Log File Info
-$sLogPath = "C:\Windows\Temp"
-$sLogName = "Migrate-Exchange.log"
+$sLogPath = '$'
+$sLogName = 'Migrate-Exchange.log'
 $sLogFile = Join-Path -Path $sLogPath -ChildPath $sLogName
 
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
@@ -51,8 +51,7 @@ Function Get-PreRequisites{
   [CmdletBinding()]
   Param(
     [parameter(Mandatory=$true)]
-    [string]$fileshare,
-    [parameter(Mandatory=$true)]
+    [string]$fileshare
   )
   
   Begin{
@@ -61,42 +60,31 @@ Function Get-PreRequisites{
   
   Process{
     Try{
-      
-      if (Test-path(!("\\$fileshare\ExchangeInstall")) {
-        New-Item -ItemType Directory -Path "\\$fileshare\ExchangeInstall"
-        Write-Verbose -Message 'Path not found, created required path'
+      $verifyPath = Test-Path -Path $fileshare
+      [int]$i = 0
+      $downloadString = @'
+https://download.microsoft.com/download/2/C/4/2C47A5C1-A1F3-4843-B9FE-84C0032C61EC/UcmaRuntimeSetup.exe
+https://download.microsoft.com/download/0/A/2/0A28BBFA-CBFA-4C03-A739-30CCA5E21659/FilterPack64bit.exe
+https://download.microsoft.com/download/A/A/3/AA345161-18B8-45AE-8DC8-DA6387264CB9/filterpack2010sp1-kb2460041-x64-fullfile-en-us.exe
+'@
+      $downloadArray = $downloadString.Split("`n",[StringSplitOptions]::RemoveEmptyEntries)
+      if (!($verifyPath)) {
+        New-Item -ItemType Directory -Path "$fileshare"
+        Write-Verbose -Message "Path not found, created required path on $fileshare"
       } else {
-        Write-Verbose -Message "Path found, downloading prerequisites to \\$fileshare\ExchangeInstall"
+        Write-Verbose -Message "Path found, downloading prerequisites to $fileshare"
       }
       
-    }
-    
-    Catch{
-      Log-Error -LogPath $sLogFile -ErrorDesc $_.Exception -ExitGracefully $True
-      Break
-    }
-  }
-  
-  End{
-    If($?){
-      Log-Write -LogPath $sLogFile -LineValue "Completed Successfully."
-      Log-Write -LogPath $sLogFile -LineValue " "
-    }
-  }
-}
-
-Function Install-Exchange{
-  Param()
-  
-  Begin{
-    Log-Write -LogPath $sLogFile -LineValue "<description of what is going on>..."
-  }
-  
-  Process{
-    Try{
-      <code goes here>
-    }
-    
+      foreach($element in $downloadArray) {
+        $i++
+        "$element Checking next $i"
+        Write-Progress -Activity 'Downloading prerequsites for Exchange 2013' -Status "Currently downloading file $element of $downloadArray.count"`
+        -PercentComplete (($i / $downloadArray.Count) * 100)
+        "$element $fileshare"
+        Start-BitsTransfer -Source $element -Destination $fileshare -Description 'Downloading prerequisites'
+        Write-Verbose -Message "Downloading file from $element"
+      }
+    }      
     Catch{
       Log-Error -LogPath $sLogFile -ErrorDesc $_.Exception -ExitGracefully $True
       Break
