38b826677b1584e787972a4147f1cc71b1999d5f
ebc92 <emil.claussen@gmail.com>
2017-03-17 12:07:51 +0100

    Implemented prerequsite check for the VM's network components.

--- Support/SMA-Provisioning.psm1
+++ Support/SMA-Provisioning.psm1
@@ -51,9 +51,10 @@ Function New-AzureStackWindowsVM {
 
         # Prerequisites
         $vnetExists = Get-AzureRmVirtualNetwork -ResourceGroupName "sma-vm-provisioning" -Name amstelvnet
-        $subnetExists = Get-AzureRmVirtualNetworkSubnetConfig -Name default -VirtualNetwork $vnet
+        $subnetExists = Get-AzureRmVirtualNetworkSubnetConfig -Name default -VirtualNetwork $vnetExists
         $nsgExists = Get-AzureRmNetworkSecurityGroup -ResourceGroupName $res -Name myNetworkSecurityGroup
         $nsRuleExists = Get-AzureRmNetworkSecurityRuleConfig -NetworkSecurityGroup $nsgExists
+        $nicExists = Get-AzureRmNetworkInterface -ResourceGroupName $res -Name NetworkConnection
 
         # Create a subnet configuration
         if(!$subnetExists){
@@ -71,27 +72,51 @@ Function New-AzureStackWindowsVM {
             Log-Write -LogPath $sLogFile -LineValue "The virtual network already exists."
         }
 
-        # Get subnet object
+        # Check if subnet configuration exists
         if(!$subnetExists){
             Log-Write -LogPath $sLogFile -LineValue "Could not get the subnet configuration."
         }
 
         # Create an inbound network security group rule for port 3389
-        $nsgRuleRDP = New-AzureRmNetworkSecurityRuleConfig -Name InboundRDP  -Protocol Tcp -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389 -Access Allow
+        if(!$nsgRuleExists){
+            $nsgRuleRDP = New-AzureRmNetworkSecurityRuleConfig -Name InboundRDP  -Protocol Tcp -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389 -Access Allow
+            Log-Write -LogPath $sLogFile -LineValue "Created network security group rule for RDP."
+        } else {
+            Log-Write -LogPath $sLogFile -LineValue "The network security group rule for RDP already exists."
+        }
 
         # Create a network security group
-        $nsg = New-AzureRmNetworkSecurityGroup -ResourceGroupName $res -Location local -Name myNetworkSecurityGroup -SecurityRules $nsgRuleRDP
+        if(!$nsgExists){
+            $nsg = New-AzureRmNetworkSecurityGroup -ResourceGroupName $res -Location local -Name myNetworkSecurityGroup -SecurityRules $nsgRuleRDP
+            Log-Write -LogPath $sLogFile -LineValue "Created network security group with RDP rules."
+        } else {
+            Log-Write -LogPath $sLogFile -LineValue "The network security group already exists."
+        }
+
+
       
         # Create a virtual network card and associate with public IP address and NSG
-        $nic = New-AzureRmNetworkInterface -ResourceGroupName $res -Location local -Name NetworkConnection -Subnet $subnet -NetworkSecurityGroup $nsg -PrivateIpAddress 192.168.58.113
+        if(!$nicExists){
+            $nic = New-AzureRmNetworkInterface -ResourceGroupName $res -Location local -Name NetworkConnection -Subnet $subnetExists -NetworkSecurityGroup $nsg -PrivateIpAddress 192.168.58.113
+            Log-Write -LogPath $sLogFile -LineValue "Created the network interface."
+        } else {
+            Log-Write -LogPath $sLogFile -LineValue "The network interface already exists."
+        }
+        
+
+        # Get the VM Image Offer
+        $offer = Get-AzureRmVMImageOffer -Location local -PublisherName MicrosoftWindowsServer
+
+        # Get the VM Image SKU
+        $sku = Get-AzureRMVMImageSku -Location local -PublisherName $offer.PublisherName -Offer $offer.Offer
 
         # Define a credential object
         $cred = Get-Credential
 
         # Create a virtual machine configuration
-        $vmConfig = New-AzureRmVMConfig -VMName myVM -VMSize Standard_D1 | `
-        Set-AzureRmVMOperatingSystem -Windows -ComputerName myVM -Credential $cred | `
-        Set-AzureRmVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus 2016-Datacenter -Version latest | `
+        $vmConfig = New-AzureRmVMConfig -VMName ProvisionVMtest -VMSize Standard_D1 | `
+        Set-AzureRmVMOperatingSystem -Windows -ComputerName VirtualTest -Credential $cred | `
+        Set-AzureRmVMSourceImage -PublisherName $offer.PublisherName -Offer $offer.Offer -Skus $sku.Skus -Version latest | `
         Add-AzureRmVMNetworkInterface -Id $nic.Id
 
         New-AzureRmVM -ResourceGroupName $res -Location local -VM $vmConfig
