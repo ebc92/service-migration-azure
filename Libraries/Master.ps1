53dcaf4e9feaee5b32ed7b646c5f70a0ae75a301
ebc92 <emil.claussen@gmail.com>
2017-04-10 11:35:47 +0200

    One script to rule them all.

---
+++ Libraries/Master.ps1
@@ -0,0 +1,25 @@
+﻿<# 
+        One script to rule them all 
+
+               Three::modules
+          for:::the::Elven-Kings
+       under:the:sky,:Seven:for:the
+     Dwarf-Lords::in::their::halls:of
+    stone,:Nine             for:Mortal
+   :::Men:::     ________     doomed::to
+ die.:One   _,-'...:... `-.    for:::the
+ ::Dark::  ,- .:::::::::::. `.   Lord::on
+his:dark ,'  .:::::zzz:::::.  `.  :throne::
+In:::the/    ::::dMMMMMb::::    \ Land::of:
+:Mordor:\    ::::dMMmgJP::::    / :where::::
+::the::: '.  '::::YMMMP::::'  ,'   Shadows:
+ lie.::One `. ``:::::::::'' ,'    :Script:
+ to:rule:    `-._```:'''_,-'     ::them::
+ all,::One      `-----'        Script:to
+   ::find:::                  them,:One
+    Script:::to            bring::them
+      all::and::in:the:darkness:bind
+        them:In:the:Land:of:Mordor
+           where:::the::Shadows
+                :::lie.:::
+#>
