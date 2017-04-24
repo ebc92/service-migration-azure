5229c764b0a19922d92cd71b686686ef06b9ef53
Gholie <nikolai.tleira@gmail.com>
2017-04-24 14:46:22 +0200

    Temporarily disabled custom logs

--- Exchange/Exchange-Migration.ps1
+++ Exchange/Exchange-Migration.ps1
@@ -82,9 +82,9 @@ Function Get-Prerequisite {
     $variableOutput = '        $fileShare ' + "= $fileShare `n"`
     +'        $tarComp ' + "= $ComputerName `n"`
     +'        $DomainCredential ' + "= $DomainCredential"
-    Log-Write -LogPath $sLogFile -LineValue "Downloading prerequisites for Microsoft Exchange 2016..."
-    Log-Write -LogPath $sLogFile -LineValue "The following variables are set for $MyInvocation.MyCommand.Name:"
-    Log-Write -LogPath $sLogFile -LineValue "$variableOutput"
+    #Log-Write -LogPath $sLogFile -LineValue "Downloading prerequisites for Microsoft Exchange 2016..."
+    #Log-Write -LogPath $sLogFile -LineValue "The following variables are set for $MyInvocation.MyCommand.Name:"
+    #Log-Write -LogPath $sLogFile -LineValue "$variableOutput"
   }
   
   Process{
@@ -101,36 +101,36 @@ Function Get-Prerequisite {
       if (!($verifyPath)) {
         New-Item -ItemType Directory -Path "$fileshare" > $null
         Write-Verbose -Message "Path not found, created required path on $fileshare" 
-        Log-Write -LogPath $sLogFile -LineValue "Path not found, created required path on $fileshare"
+        #Log-Write -LogPath $sLogFile -LineValue "Path not found, created required path on $fileshare"
       } else {
         Write-Verbose -Message "Path found on $fileshare"
-        Log-Write -LogPath $sLogFile -LineValue "Path found on $fileShare"
+        #Log-Write -LogPath $sLogFile -LineValue "Path found on $fileShare"
       }
       
       #Checking if NuGet is in the package provider list, and installing it if it's not
       if (!($nuget)) {
         Write-Verbose -Message "NuGet not installed, installing now..."
-        Log-Write -LogPath $sLogFile -LineValue "Nuget not installed, installing now..."
+        #Log-Write -LogPath $sLogFile -LineValue "Nuget not installed, installing now..."
         Install-PackageProvider -Name NuGet -Force
       } else {
         Write-Verbose -Message "NuGet already installed, continuing prerequisite checks"
-        Log-Write -LogPath $sLogFile -LineValue "NuGet already installed, continuing prerequisite checks"
+        #Log-Write -LogPath $sLogFile -LineValue "NuGet already installed, continuing prerequisite checks"
       }  
           
       #Downloading UCMA 4.0 Runtime      
       Write-Verbose -Message "Starting download of UCMA Runtime 4.0"
-      Log-Write -LogPath $sLogFile -LineValue "Starting download of UCMA Runtime 4.0"
+      #Log-Write -LogPath $sLogFile -LineValue "Starting download of UCMA Runtime 4.0"
 
       Start-BitsTransfer -Source https://download.microsoft.com/download/2/C/4/2C47A5C1-A1F3-4843-B9FE-84C0032C61EC/UcmaRuntimeSetup.exe -Destination $fileshare -Description 'Downloading prerequisites'
       Write-Verbose -Message "Downloading file from https://download.microsoft.com/download/2/C/4/2C47A5C1-A1F3-4843-B9FE-84C0032C61EC/UcmaRuntimeSetup.exe to $fileShare"
-      Log-Write -LogPath $sLogFile -LineValue "Downloading file from https://download.microsoft.com/download/2/C/4/2C47A5C1-A1F3-4843-B9FE-84C0032C61EC/UcmaRuntimeSetup.exe to $fileShare"
+      #Log-Write -LogPath $sLogFile -LineValue "Downloading file from https://download.microsoft.com/download/2/C/4/2C47A5C1-A1F3-4843-B9FE-84C0032C61EC/UcmaRuntimeSetup.exe to $fileShare"
 
       #Downloading Exchange 2016
-      Write-Verbose -Message "Starting download of Exchange 2016 CU5"
+      #Write-Verbose -Message "Starting download of Exchange 2016 CU5"
       Log-Write -LogPath $sLogFile -LineValue "Starting download of Exchange 2016 CU5"
       Start-BitsTransfer -Source https://download.microsoft.com/download/A/A/7/AA7F69B2-9E25-4073-8945-E4B16E827B7A/ExchangeServer2016-x64-cu5.iso -Destination $fileshare -Description 'Downloading prerequisites'
       Write-Verbose -Message "Downloading file from https://download.microsoft.com/download/A/A/7/AA7F69B2-9E25-4073-8945-E4B16E827B7A/ExchangeServer2016-x64-cu5.iso to $fileShare"
-      Log-Write -LogPath $sLogFile -LineValue "Downloading file from https://download.microsoft.com/download/A/A/7/AA7F69B2-9E25-4073-8945-E4B16E827B7A/ExchangeServer2016-x64-cu5.iso to $fileShare"
+      #Log-Write -LogPath $sLogFile -LineValue "Downloading file from https://download.microsoft.com/download/A/A/7/AA7F69B2-9E25-4073-8945-E4B16E827B7A/ExchangeServer2016-x64-cu5.iso to $fileShare"
 
     }     
     Catch {
@@ -141,8 +141,8 @@ Function Get-Prerequisite {
   
   End{
     If($?){
-      Log-Write -LogPath $sLogFile -LineValue "Got all prerequisites successfully."
-      Log-Write -LogPath $sLogFile -LineValue " "
+      #Log-Write -LogPath $sLogFile -LineValue "Got all prerequisites successfully."
+      #Log-Write -LogPath $sLogFile -LineValue " "
       Write-Verbose -Message 'Got all prerequisites successfully.'
     }
   }
@@ -189,9 +189,9 @@ Function Install-Prerequisite {
   
   Begin{
     $variableOutput = '        $fileShare ' + "= $fileShare"
-    Log-Write -LogPath $sLogFile -LineValue 'Installing prerequisites for Microsoft Exchange 2013...'
-    Log-Write -LogPath $sLogFile -LineValue "The following variables are set for $MyInvocation.MyCommand.Name :"
-    Log-Write -LogPath $sLogFile -LineValue "$variableOutput"
+    #Log-Write -LogPath $sLogFile -LineValue 'Installing prerequisites for Microsoft Exchange 2013...'
+    #Log-Write -LogPath $sLogFile -LineValue "The following variables are set for $MyInvocation.MyCommand.Name :"
+    #Log-Write -LogPath $sLogFile -LineValue "$variableOutput"
   }
   
   Process{
@@ -202,7 +202,7 @@ Function Install-Prerequisite {
       $Domain = $env:USERDOMAIN
       
       Write-Verbose -Message "Total amount of files to be installed is $total, starting installation"
-      Log-Write -LogPath $sLogPath -LineValue "Total amount of files to be installed is $total, starting installation"
+      #Log-Write -LogPath $sLogPath -LineValue "Total amount of files to be installed is $total, starting installation"
 
       Install-Module -Name xExchange, xPendingReboot, xWindowsUpdate
 
@@ -249,8 +249,8 @@ Function Install-Prerequisite {
   
   End{
     If($?){
-      Log-Write -LogPath $sLogFile -LineValue "Installed prerequisites successfully."
-      Log-Write -LogPath $sLogFile -LineValue ""
+      #Log-Write -LogPath $sLogFile -LineValue "Installed prerequisites successfully."
+      #Log-Write -LogPath $sLogFile -LineValue ""
       Write-Verbose -Message "Installed prerequisites successfully."
     }
   }
@@ -284,7 +284,7 @@ Function Install-Prerequisite {
 
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 
-Log-Start -LogPath $sLogPath -LogName $sLogName -ScriptVersion $sScriptVersion
+#Log-Start -LogPath $sLogPath -LogName $sLogName -ScriptVersion $sScriptVersion
 $i = 0
 
 #Temporary to run commands during test environment
@@ -296,4 +296,4 @@ Mount-Exchange -SourceFile $fileshare
 
 Install-Prerequisite -fileShare $fileshare -ComputerName 192.168.58.116 -DomainCredential $cred -ExchangeBinary $ExchangeBinary
 
-Log-Finish -LogPath $sLogFile
+#Log-Finish -LogPath $sLogFile
