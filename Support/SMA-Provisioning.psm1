da9b10d0c4f1ace6d606b9d8b2994543a55b2980
ebc92 <emil.claussen@gmail.com>
2017-05-04 17:35:40 +0200

    why

--- Support/SMA-Provisioning.psm1
+++ Support/SMA-Provisioning.psm1
@@ -190,12 +190,19 @@ Function New-AzureStackVnet{
 
         # Create a virtual network card and associate with public IP address and NSG
         if(!$nic){   
-            Log-Write -LogPath $sLogFile -LineValue "Here comes the sun"
+            Log-Write -LogPath $sLogFile -LineValue "Updating the subnet configuration.."
             $subnet = Get-AzureRmVirtualNetworkSubnetConfig -Name "HostSubnet" -VirtualNetwork $vnet
-            Log-Write -LogPath $sLogFile -LineValue "Creating ipconfig.."
-            $IPconfig = New-AzureRmNetworkInterfaceIpConfig -Name $VMNicName -PrivateIpAddressVersion IPv4 -PrivateIpAddress $Network.Address -Subnet $subnet
+            #Log-Write -LogPath $sLogFile -LineValue "Creating ipconfig.."
+            #$IPconfig = New-AzureRmNetworkInterfaceIpConfig -Name $VMNicName -PrivateIpAddressVersion IPv4 -PrivateIpAddress $Network.Address -Subnet $subnet
+            $res | Out-String > .\debug.txt
+            $Location | Out-String >> .\debug.txt
+            $VMNicName | Out-String >> .\debug.txt
+            $nsg | Out-String >> .\debug.txt
+            $subnet | Out-String >> .\debug.txt
+            $Network.Address | Out-String >> .\debug.txt
             Log-Write -LogPath $sLogFile -LineValue "Creating interface.."
-            $nic = New-AzureRmNetworkInterface -ResourceGroupName $res -Location $Location -Name $VMNicName -NetworkSecurityGroup $nsg -IpConfigurationName $VMNicName -ErrorAction Stop
+            $nic = New-AzureRmNetworkInterface -ResourceGroupName $res -Location $Location -Name $VMNicName -NetworkSecurityGroup $nsg -Subnet $subnet -PrivateIpAddress $Network.Address -DnsServer "8.8.8.8","4.4.4.4" -ErrorAction Stop
+            $nic | Out-String >> .\debug.txt
             Log-Write -LogPath $sLogFile -LineValue "Created the network interface."
         } else {
             Log-Write -LogPath $sLogFile -LineValue "The network interface already exists."
