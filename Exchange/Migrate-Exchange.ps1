ebd670e9236c7d9629604530b9115d5be95c0987
Gholie <nikolai.tleira@gmail.com>
2017-05-07 21:07:44 +0200

    Made use of configuration.ini to load all required parameters

--- Exchange/Migrate-Exchange.ps1
+++ Exchange/Migrate-Exchange.ps1
@@ -1,1 +1,34 @@
-﻿
+﻿Param(
+  [pscredential]$DomainCredential
+)
+
+#Sets data to all parameters from configuration.ini
+$fileshare = $SMAConfig.Exchange.fileshare
+$baseDir = $SMAConfig.Exchange.basedir
+$ComputerName = $SMAConfig.Exchange.destination
+$SourceComputer = $SMAConfig.Exchange.source
+$fqdn = $SMAConfig.Exchange.fqdn
+$newfqdn = $SMAConfig.Exchange.newfqdn
+$www = $SMAConfig.Exchange.www
+$Password = (ConvertTo-SecureString $SMAConfig.Exchange.password -AsPlainText -Force)
+
+
+
+
+Get-Prerequisite -fileShare $fileshare -ComputerName -DomainCredential 
+
+#Mount fileshare on source VM
+Mount-FileShare -DomainCredential -ComputerName -baseDir
+
+#Mount fileshare on target VM
+Mount-FileShare -DomainCredential -ComputerName -baseDir
+
+Mount-Exchange -FileShare -ComputerName -baseDir -DomainCredential
+
+New-DSCCertificate -ComputerName -DomainCredential
+
+Install-Prerequisite -baseDir -ComputerName -DomainCredential
+
+Export-ExchCert -SourceComputer -fqdn -Password -DomainCredential
+
+Configure-Exchange -ComputerName -SourceComputer -newfqdn -Password -DomainCredential -hostname
