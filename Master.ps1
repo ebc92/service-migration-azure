95996b42ca3ba220b0e6830830f19acb0fdd5192
Gholie <nikolai.tleira@gmail.com>
2017-05-20 12:38:42 +0200

    Added comments to the Master script

--- Master.ps1
+++ Master.ps1
@@ -46,27 +46,34 @@ $functions | % {
 }
 
 #---------------------------------------------------------[Load credentials]--------------------------------------------------------
+#The Azure Stack Credential. Azurestack/<AdministratorAccount>
 if(!$AzureLocalCredential){
   $AzureLocalCredential = (Get-Credential -Message "Please insert your Local AzureStack Credentials")
 }
 
+#The Azure AD Administrator tenant <AdministratorAccount>@<AADDomain>.onmicrosoft.com
 if(!$AzureTenantCredential){
   $AzureTenantCredential = (Get-Credential -Message "Please insert your Azure Tenant Credentials")
 }
 
+#The Domain Administrator account
 if(!$DomainCredential){
     $global:DomainCredential = (Get-Credential -Message "Please insert your domain administrator credentials")
 }
 
+#The Local SQL instance account for SQL authentication
 if(!$SqlCredential){
     $global:SqlCredential = (Get-Credential -Message "Please insert a password for SQL Authentication")
 }
 
+#The local administartor account on the new VMs, can be set to any account
+#Remember to note it down in case it is needed in the future
 if(!$VMCredential){ #TODO: Pass these to vm provisioning
     $global:VMCredential = (Get-Credential -Message "Please insert a password for the local administrator on the new VMs")
 }
 
-#----------------------------------------------------------[Global Declarations]----------------------------------------------------------
+#----------------------------------------------------------[Global Declarations]----------------------------------------------------
+#Sets the global variables that are used, mostly for logging purposes. Also loads the Configuration.ini file to the $SMAConfig variable
 $global:SMAConfig = Get-IniContent -FilePath (Join-Path -Path $PSScriptRoot -ChildPath "Configuration.ini")
 
 $sLogPath = $SMAConfig.Global.logpath
@@ -84,8 +91,10 @@ $m = "Starting service migration execution.."
 Log-Write -LogPath $sLogFile -LineValue $m
 Write-Verbose $m
 
+#Loads all the modules into an array.
 $module = @("ADDC\ADDC-Migration.psm1", "MSSQL\MSSQL-Migration.psm1", "Support\SMA-Provisioning.psm1", "File-Share\Move-File.psm1", "Exchange\ExchangeMigrate\loader.psm1")
 
+#Goes through each module in the array, and then imports it.
 $module | % {
     Try {
         Import-Module (Join-Path -Path $PSScriptRoot -ChildPath $_) -Force -ErrorAction Stop
@@ -113,6 +122,7 @@ $Authenticator = Join-Path -Path $PSScriptRoot -ChildPath "\Support\Remote-ARM\S
 $ADDCName = "$($environmentname)-$($SMAConfig.MSSQL.hostname)"
 $ADDCDestination = $SMAConfig.MSSQL.destination + $CIDR
 
+#Runs the required script to start the migration of the service and deployment of the new VM
 Invoke-Command -Session $AzureStackSession -ScriptBlock {New-AzureStackTenantDeployment -VMName $using:ADDCName -IPAddress $using:ADDCDestination -DomainCredential $using:DomainCredential}
 & (Join-Path -Path $PSScriptRoot -ChildPath "\ADDC\ADDC-Migration.ps1")
 
@@ -120,6 +130,7 @@ Invoke-Command -Session $AzureStackSession -ScriptBlock {New-AzureStackTenantDep
 $FSSName = "$($environmentname)-$($SMAConfig.FSS.hostname)"
 $FSSNewIP = $SMAConfig.FSS.newip + $CIDR
 
+#Runs the required script to start the migration of the service and deployment of the new VM
 Invoke-Command -Session $AzureStackSession -ScriptBlock {New-AzureStackTenantDeployment -VMName $using:FSSName -IPAddress $using:FSSNewIP -DomainCredential $using:DomainCredential}
 & (Join-Path -Path $PSScriptRoot -ChildPath "\File-Share\Migrate-FSS.ps1") $DomainCredential
 
@@ -127,6 +138,7 @@ Invoke-Command -Session $AzureStackSession -ScriptBlock {New-AzureStackTenantDep
 $SQLName = "$($environmentname)-$($SMAConfig.MSSQL.hostname)"
 $SQLDestination = $SMAConfig.MSSQL.destination + $CIDR
 
+#Runs the required script to start the migration of the service and deployment of the new VM
 Invoke-Command -Session $AzureStackSession -ScriptBlock {New-AzureStackTenantDeployment -VMName $using:SQLName -IPAddress $using:SQLDestination -DomainCredential $using:DomainCredential}
 & (Join-Path -Path $PSScriptRoot -ChildPath "\MSSQL\MSSQL-Migration.ps1")
 
@@ -134,6 +146,7 @@ Invoke-Command -Session $AzureStackSession -ScriptBlock {New-AzureStackTenantDep
 $ExchName = "$($environmentname)-$($SMAConfig.Exchange.hostname)"
 $ExchNewIp = $SMAConfig.Exchange.newip + $CIDR
 
+#Runs the required script to start the migration of the service and deployment of the new VM
 Invoke-Command -Session $AzureStackSession -ScriptBlock {New-AzureStackTenantDeployment -VMName $using:ExchName -IPAddress $using:ExchNewIp -DomainCredential $using:DomainCredential}
 & (Join-Path -Path $PSScriptRoot -ChildPath "\Exchange\Migrate-Exchange.ps1") $DomainCredential
 
